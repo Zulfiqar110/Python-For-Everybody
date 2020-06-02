21b3c49b5877c6b690d1e064e5f03c30afac87b9
Zulfiqar Ali mirza <55847311+Zulfiqar110@users.noreply.github.com>
2020-06-03 01:11:27 +0530

    Add files via upload

---
+++ where.js
@@ -0,0 +1,12 @@
+myData = [
+[19.0595596,72.8295287, 'Bandra West, Mumbai, Maharashtra, India'],
+[19.2183307,72.9780897, 'Thane, Maharashtra, India'],
+[19.2087139,72.9715174, 'On Eastern Express Highway, Service Rd, Laxmi Nagar, Thane West, Thane, Maharashtra 400601, India'],
+[19.2859416,72.8695487, '#H-11, Latiff Park, Mira Bhayandar Rd, opp. S.K. Stone, Chandan Shanti, Mira Road, Mumbai, Maharashtra 401107, India'],
+[50.06688579999999,19.9136192, 'aleja Adama Mickiewicza 30, 30-059 Kraków, Poland'],
+[52.2394019,21.0150792, 'Krakowskie Przedmieście 5, 00-068 Warszawa, Poland'],
+[30.018923,31.499674, 'AUC Avenue، Road، First New Cairo, Cairo Governorate 11835, Egypt'],
+[33.4242399,-111.9280527, 'Tempe, AZ 85281, USA'],
+[33.9094132,-83.4603953, '2500 Daniells Bridge Rd #300, Athens, GA 30606, USA'],
+[28.3588163,75.58802039999999, 'Vidya Vihar, Pilani, Rajasthan 333031, India']
+];
